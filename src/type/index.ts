1dc87cad1cda8a50a81aa068576f5f9a2737ba29
root <root@rag-service-india.asia-south1-b.c.via-socket-prod.internal>
2025-12-01 04:11:47 +0000

    Removed unused type declaration

--- src/type/index.ts
+++ src/type/index.ts
@@ -1,12 +1,2 @@
-import { User } from "./user";
 
-declare global {
-    namespace Express {
-        interface Response {
-            locals: {
-                user?: User;
-            }
-        }
-    }
-}
 
