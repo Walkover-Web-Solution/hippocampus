bf4eb404c93c6a1f4a8ae2e49d4663717090bacd
Ankit Kumar <ankitkumar@whozzat.com>
2025-12-16 17:17:05 +0000

    Feat: first draft of human feedback integration is done

--- src/route/search/index.ts
+++ src/route/search/index.ts
@@ -3,6 +3,7 @@ import { ApiError } from '../../error/api-error';
 import { search } from '../../service/search';
 import { v4 as uuidv4 } from 'uuid';
 import redis from '../../config/redis';
+import feedbackService from '../../service/feedback';
 
 const router = express.Router();
 
