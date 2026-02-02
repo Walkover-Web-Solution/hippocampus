ee301f6722ae1ef92b2421c4df347788074d8ea6
Ankit Kumar <ankitkumar@whozzat.com>
2026-02-02 17:46:00 +0000

    Added retry mechanisam in axios

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -12,4 +12,34 @@ const axiosInstance = axios.create({
     httpsAgent: httpsAgent
 });
 
+axiosInstance.interceptors.response.use(
+    response => response, // Return successful responses directly
+    async error => {
+        const config = error.config;
+
+        // If config does not exist or the retry option is not set, reject
+        if (!config || !config.retry) return Promise.reject(error);
+
+        // Check if we have retried the max amount
+        config.__retryCount = config.__retryCount || 0;
+        if (config.__retryCount >= config.retry) return Promise.reject(error);
+
+        // Condition for retry
+        const shouldRetry = (error.response && error.response.status >= 500) || error.code === 'ECONNRESET';
+        if (shouldRetry) {
+            config.__retryCount += 1;
+
+            // Create a delay (backoff)
+            const backoff = new Promise(resolve => {
+                setTimeout(() => resolve(true), 1000 * config.__retryCount);
+            });
+
+            await backoff;
+            return axiosInstance(config);
+        }
+
+        return Promise.reject(error);
+    }
+);
+
 export default axiosInstance;
